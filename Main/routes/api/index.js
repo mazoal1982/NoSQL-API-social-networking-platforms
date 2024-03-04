250f886d7d4cd554f73cad53da87aaec22746fd3
mazoal1982 <aloys.manga@gmail.com>
2024-03-04 14:14:29 -0700

    added routes

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const thoughtsRoutes = require('./thoughtsRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/thoughts', thoughtsRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
---
+++ Main/routes/api/thoughtsRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtsController');
+
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
+
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+.post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(removeReaction);
+
+module.exports = router;
+
